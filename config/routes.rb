42d8af1a52a3ca7eb0c98dd83df7c31e04744fcb
jaxa-TS <jaxa-TS@users.noreply.github.com>
2021-01-22 14:36:11 +0900

    six commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
-  get 'passwords/edit'
-  get 'accounts/show'
-  get 'accounts/edit'
   root "top#index"
-  get"about" =>"top#about",as:"about"
+  get "about" => "top#about", as: "about"
 
   1.upto(18) do |n|
-    get "lesson/step#{n}(/:name)"=>"lesson#step#{n}"
+    get "lesson/step#{n}(/:name)" => "lesson#step#{n}"
   end
+
   resources :members do
     get "search", on: :collection
+    resources :entries, only: [:index]
   end
-  resource:session, only:[:create, :destroy]
-  resource :account,only:[:show,:edit,:update]
-  resource :password,only:[:show,:edit,:update]
+
+  resource :session, only: [:create, :destroy]
+  resource :account, only: [:show, :edit, :update]
+  resource :password, only: [:show, :edit, :update]
+
   resources :articles
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :entries
 end
